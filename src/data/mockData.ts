cf93e7c59b414cb239cedfbe90497c166943b1a7
Joanna Leidy <blue.joanna@gmail.com>
2025-02-09 14:52:59 -0500

    Style tweaks to add scroll bars for long content

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -58,6 +58,86 @@ export const MOCK_FILE_SYSTEM: ITreeNode[] = [{
       name: "dogphoto4.jpg",
       modified: new Date("2024-04-22T11:34:00"),
       size: 1234
+    }, {
+      type: "file",
+      name: "dogphoto5.jpg",
+      modified: new Date("2024-04-22T11:14:00"),
+      size: 235
+    }, {
+      type: "file",
+      name: "dogphoto6.jpg",
+      modified: new Date("2024-04-22T11:17:00"),
+      size: 2200
+    }, {
+      type: "file",
+      name: "dogphoto7.jpg",
+      modified: new Date("2024-04-22T11:24:00"),
+      size: 240
+    }, {
+      type: "file",
+      name: "dogphoto8.jpg",
+      modified: new Date("2024-04-22T11:34:00"),
+      size: 1234
+    }, {
+      type: "file",
+      name: "dogphoto9.jpg",
+      modified: new Date("2024-04-22T11:14:00"),
+      size: 235
+    }, {
+      type: "file",
+      name: "dogphoto10.jpg",
+      modified: new Date("2024-04-22T11:17:00"),
+      size: 2200
+    }, {
+      type: "file",
+      name: "dogphoto11.jpg",
+      modified: new Date("2024-04-22T11:24:00"),
+      size: 240
+    }, {
+      type: "file",
+      name: "dogphoto12.jpg",
+      modified: new Date("2024-04-22T11:34:00"),
+      size: 1234
+    }, {
+      type: "file",
+      name: "dogphoto13.jpg",
+      modified: new Date("2024-04-22T11:14:00"),
+      size: 235
+    }, {
+      type: "file",
+      name: "dogphoto14.jpg",
+      modified: new Date("2024-04-22T11:17:00"),
+      size: 2200
+    }, {
+      type: "file",
+      name: "dogphoto15.jpg",
+      modified: new Date("2024-04-22T11:24:00"),
+      size: 240
+    }, {
+      type: "file",
+      name: "dogphoto16.jpg",
+      modified: new Date("2024-04-22T11:34:00"),
+      size: 1234
+    }, {
+      type: "file",
+      name: "dogphoto17.jpg",
+      modified: new Date("2024-04-22T11:24:00"),
+      size: 240
+    }, {
+      type: "file",
+      name: "dogphoto18.jpg",
+      modified: new Date("2024-04-22T11:34:00"),
+      size: 1234
+    }, {
+      type: "file",
+      name: "dogphoto19.jpg",
+      modified: new Date("2024-04-22T11:14:00"),
+      size: 235
+    }, {
+      type: "file",
+      name: "dogphoto20.jpg",
+      modified: new Date("2024-04-22T11:17:00"),
+      size: 2200
     }]
   },
     {
@@ -69,7 +149,19 @@ export const MOCK_FILE_SYSTEM: ITreeNode[] = [{
         type: "folder",
         name: "Applications",
         modified: new Date("2021-12-17T03:24:00"),
-        size: 0
+        size: 0,
+        children: [{
+          type: "folder",
+          name: "Games",
+          modified: new Date("2022-05-17T08:56:00"),
+          size: 0,
+          children: [{
+            type: "file",
+            name: "Goat Simulator.exe",
+            modified: new Date("2022-06-11T14:56:00"),
+            size: 123458
+          }]
+        }]
       }, {
         type: "folder",
         name: "Library",
